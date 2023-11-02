d92759965b7897f1c15958c8037fd28f922f9ee8
Jeiojey <signofaries@gmail.com>
2023-11-02 16:35:40 +0300

    feat: add header comp

---
+++ Vue.js/interior-studio-cli/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <TheHeader />
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader
+  }
+}
+</script>
+
+<style lang="scss">
+//colors
+$black: #292F36;
+$gray: #4D5053;
+
+//Content-width
+$content-width: 1200px;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  font-family: 'Jost', sans-serif;
+}
+
+.visually_hidden {
+  display: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+a:hover {
+  color: #CDA274;
+}
+
+img {
+  max-width: 100%;
+}
+
+ul {
+  list-style-type: none;
+
+}
+
+.center {
+  max-width: $content-width;
+  margin: 0 auto;
+}
+
+.content {
+  padding-left: 2%;
+  padding-right: 2%;
+}
+
+.heading {
+  font-family: 'DM Serif Display', serif;
+  font-style: normal;
+  color: $black;
+  font-weight: 400;
+  text-align: center;
+  font-size: 50px;
+  line-height: 125%;
+  letter-spacing: 1px;
+}
+
+.text {
+  color: $gray;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%;
+  letter-spacing: 0.22px;
+}
+</style>
---
+++ Vue.js/interior-studio-cli/src/components/TheHeader.vue
@@ -0,0 +1,42 @@
+<template lang="">
+  <div>
+    <header class="header center">
+        <a href="index.html"><img src="../assets/index/Logo.svg" alt="logotype" class="logo"></a>
+        <nav class="header__nav">
+            <ul class="header__nav_menu">
+                <li class="header__nav_menu_item"><a class="header__nav_menu_link" href="./index.html">Home</a></li>
+                <li class="header__nav_menu_item"><a class="header__nav_menu_link" href="#">Project</a></li>
+                <li class="header__nav_menu_item"><a class="header__nav_menu_link" href="./blog.html">Blog</a></li>
+            </ul>
+        </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader'
+}
+</script>
+
+<style lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  width: 1200px;
+  padding-top: 53px;
+  padding-bottom: 57px;
+
+  &__nav_menu {
+    display: flex;
+    justify-content: space-between;
+    width: 243px;
+
+    &_link {
+      font-size: 20px;
+      color: black;
+      cursor: pointer;
+    }
+  }
+}
+</style>
